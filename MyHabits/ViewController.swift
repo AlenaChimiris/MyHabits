21c0075f8bec98f0da5c7045ed9d4876f5410b01
Alena Chimiris <achimiris@list.ru>
2021-01-04 13:20:53 +0300

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Алена Чимирис on 04.01.2021.
+//  Copyright © 2021 Алена Чимирис. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
